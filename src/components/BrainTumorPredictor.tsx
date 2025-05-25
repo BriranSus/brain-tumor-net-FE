63b97c58485e68b1ba3f5855c03c752837823270
BriranSus <alexander.susanto001@binus.ac.id>
2025-05-25 14:37:20 +0700

    V1 Patch Update

--- src/components/BrainTumorPredictor.tsx
+++ src/components/BrainTumorPredictor.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { Client } from "@gradio/client";
 import brain from "../assets/brain.svg";
+import upload from "../assets/upload.svg";
+import checkmark from "../assets/checkmark.svg";
+import exclamationmark from "../assets/exclamationmark.svg";
 
 const BrainTumorPredictor: React.FC = () => {
   const [predictResult, setPredictResult] = useState<any>(null);
@@ -37,6 +40,12 @@ const BrainTumorPredictor: React.FC = () => {
     }
   };
 
+  const handleRemoveImage = async () => {
+    setPreviewUrl(null);
+    setImageFile(null);
+    setPredictResult(null);
+  };
+
   return (
     <div className="flex flex-col items-center">
       <header className="bg-dark_grey border-b min-w-screen mb-[2rem]">
@@ -46,33 +55,69 @@ const BrainTumorPredictor: React.FC = () => {
         </div>
       </header>
 
-      <h2>Brain Tumor Prediction</h2>
+      <div className="flex flex-row ">
+        <div className="flex flex-col gap-[1rem] items-center justify-center mr-[2rem]">
 
-      <div className="flex flex-col gap-[1rem] items-center justify-center min-w-screen">
-        <input 
-          type="file" 
-          accept="image/*" 
-          onChange={handleImageChange}
-          className="text-[18px] bg-grey border-2 border-grey rounded-[5px] pl-[2rem] py-[6rem] cursor-pointer inset-shadow-[2px,6px,6px,rgba(0,0,0,0.25)] hover:border-light_grey hover:ring-2 hover:ring-grey focus:ring-2 focus:ring-grey" />
-        
-        {previewUrl && (
-          <img src={previewUrl} alt="" className="size-[12rem]"/>
-        )}
+          <label>
+            {previewUrl ? <img src={previewUrl} className="size-[15.5rem] border px-[4rem] py-[3rem]" /> : 
+              <div className="min-w-[340px] text-[18px] bg-white border-2 border-dashed border-grey rounded-[5px] px-[1rem] py-[6rem] cursor-pointer inset-shadow-[2px,6px,6px,rgba(0,0,0,0.25)] hover:border-light_grey hover:ring-2 hover:ring-grey focus:ring-2 focus:ring-grey text-center justify-center">
+                <img src={upload} alt="" className="size-[48px]" />
+                <p className="font-[600]">Click to upload your MRI scan here</p>
+              </div>
+            }
+            <input 
+              type="file"
+              accept="image/*" 
+              onChange={handleImageChange} 
+              hidden />
+          </label>
 
-        <button 
-          onClick={handlePredict} 
-          disabled={loading || !imageFile}
-          className="min-w-[380px] text-center justify-center text-[18px] py-[1rem] border-2 border-dark_grey rounded-[5px] inset-shadow-[2px,6px,6px,rgba(0,0,0,0.25)] hover:border-grey hover:ring-2 hover:ring-dark_grey focus:ring-2 focus:ring-dark_grey cursor-pointer">
-          {loading ? "Predicting..." : "Predict"}
-        </button>
+          {previewUrl && (
+            <button
+              onClick={handleRemoveImage}
+              className="absolute ml-[21rem] mb-[23rem] border rounded-[5px] text-background bg-white text-[18px] cursor-pointer hover:bg-light_grey px-[8px]"
+            >
+              x
+            </button>
+          )}
+
+          <button 
+            onClick={handlePredict} 
+            disabled={loading || !imageFile}
+            className="min-w-[380px] text-center justify-center text-[18px] py-[1rem] border-2 border-dark_grey rounded-[5px] inset-shadow-[2px,6px,6px,rgba(0,0,0,0.25)] hover:border-grey hover:ring-2 hover:ring-dark_grey focus:ring-2 focus:ring-dark_grey cursor-pointer">
+            {loading ? "Loading..." : "Analyze MRI Scan"}
+          </button>
+
+        </div> 
+
+        <div className="border border-grey min-w-[370px] min-h-[300px] flex flex-row justify-center items-center bg-dark_grey">
+          {!predictResult && (
+            <div>
+              <p>Upload an MRI scan to see prediction results</p>
+            </div>
+          )}
+          {predictResult && (
+            <div>
+              {JSON.stringify(predictResult[0].label) === '"No Tumor"' ? (
+                <div className="flex flex-col justify-center items-center">
+                  <img src={checkmark} className="size-[60px] bg-light_ijo rounded-[60px] p-[12px]" alt="" />
+                  <p className="text-[28px] text-center text-ijo">{JSON.stringify(predictResult[0].label)} Detected</p>
+                </div>
+              ) : (
+                <div className="flex flex-col justify-center items-center">
+                  <img src={exclamationmark} className="size-[48px] bg-light_red rounded-[60px] p-[16px]" alt="" />
+                  <p className="text-[28px] text-center text-[red]">{JSON.stringify(predictResult[0].label)} Detected</p>
+                </div>
+              )} 
+            </div>
+          )}  
+        </div>
+
       </div>
 
-      {predictResult && (
-        <div>
-          <h3>Predict Result:</h3>
-          <p>{JSON.stringify(predictResult)}</p>
-        </div>
-      )}
+       
+
+      
 
     </div>
   );
